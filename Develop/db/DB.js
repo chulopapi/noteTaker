25ef715081a7d1f72ded0563ee9792dd295e055b
chulopapi <marlon.guandique@gmail.com>
2020-08-20 11:49:18 -0700

    adding DB.js that handles write and read notes

---
+++ Develop/db/DB.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const util = require("util");
+//allow to chain promisses
+const readFAsync = util.promisify(fs.readFile);
+const writeFAsync = util.promisify(fs.writeFile);
+class DB {
+  readFile() {
+    return readFAsync("./db/db.json", "utf8");
+  }
+  // This is fs.writeFile(file,data[,options],callback)
+  writeFile(rawNote) {
+    return writeFAsync("./db/db.json", JSON.stringify(rawNote), "utf8");
+  }
+
+  readNotes() {
+    return this.readFile().then((notes) => {
+      return JSON.parse(notes);
+    });
+  }
+
+  writeNote(newNote) {
+    console.log("NEW NOTE=>", newNote);
+    return this.writeFile(newNote).then((notes) => {
+      return this.readNotes();
+    });
+  }
+
+  removeNote(id) {
+      console.log("remoiving note with id: ", id)
+      return this.readNotes()
+      .then(notes => notes.filter(note => note.id != id))
+      .then(fNotes => this.writeFile(fNotes))
+  }
+}
